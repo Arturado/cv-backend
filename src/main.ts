7dc73d583fcd30071920ae905a26b0c3f855862b
Arturo Vasquez <arturados@gmail.com>
2025-07-25 07:15:40 -0400

    Cambio en el Cors para Vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', // o ['http://localhost:5173'] para limitar
+    origin: ['http://localhost:5173', 'https://arturado-cv.vercel.app'], // o ['http://localhost:5173'] para limitar
   });
   await app.listen(process.env.PORT ?? 3000);
 }
