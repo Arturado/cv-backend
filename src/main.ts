da0cd1406081689d1f6838b970589b4776252b7d
Arturo Vasquez <arturados@gmail.com>
2025-07-25 06:31:55 -0400

    Cambio de Cors para conectarme desde Local

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // o ['http://localhost:5173'] para limitar
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
